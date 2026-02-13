9be0a9c5734af5b5b866cf3fcea862dc12079510
Alexandr <stitel23@mail.ru>
2026-02-13 12:57:18 +0300

    More cards and randomUUID

--- products-spa/src/api/petsApi.ts
+++ products-spa/src/api/petsApi.ts
@@ -1,11 +1,11 @@
 import type { Pet } from '../features/pets/types';
 
 export async function fetchPets(): Promise<Pet[]> {
-  const res = await fetch('https://dog.ceo/api/breeds/image/random/12');
+  const res = await fetch('https://dog.ceo/api/breeds/image/random/30');
   const data = await res.json();
 
   return data.message.map((url: string, index: number) => ({
-    id: String(index),
+    id: crypto.randomUUID(),
     title: `Dog #${index + 1}`,
     description: 'Cute dog image!',
     image: url,
